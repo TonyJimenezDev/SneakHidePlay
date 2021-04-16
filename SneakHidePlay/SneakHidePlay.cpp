99c62b0424d677b85a97ffbbd175ceb733335141
Tony Jimenez <TonyJimenezDev@outlook.com>
2021-04-16 00:31:40 -0500

    Add project files.

---
+++ SneakHidePlay/SneakHidePlay.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{	
+	std::cout << "you are a secret agent breaking into a secure server room\n";
+	std::cout << "you need to enter the correct codes to continue\n";
+
+	return 0;
+}
+
+
