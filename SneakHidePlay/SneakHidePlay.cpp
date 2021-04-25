648880eea8b78b4e0e6c0f9cea8a4411987b88d2
Tony Jimenez <TonyJimenezDev@outlook.com>
2021-04-25 13:14:13 -0500

    Game Completed. A few more hints could be added to make it easier. Also could add more to gameover and gamewon

--- SneakHidePlay/SneakHidePlay.cpp
+++ SneakHidePlay/SneakHidePlay.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <cTime>
 #include <Windows.h>
+#include <string>
 
 #pragma region Global variables
 HANDLE coutHandle = GetStdHandle(STD_OUTPUT_HANDLE);
 int levelDiffuclty = 1;
 char codeName[20];
 int age;
+int playerGuesses = 0;
 #pragma endregion
 
-
+#pragma region Utilities
 int CheckIfNumber(int number, const char* question)
 {
 	std::cin >> number;
@@ -25,52 +27,138 @@ int CheckIfNumber(int number, const char* question)
 	return number;
 }
 
+void SlowDownText(const std::string str, int delayTime)
+{
+	for (int i = 0; i != str.size(); i++)
+	{
+		std::cout << str[i];
+		Sleep(delayTime);
+	}
+}
+
+void ClearScreen(int delayTime)
+{
+	Sleep(delayTime);
+	std::cout << "\x1B[2J\x1B[H";
+}
+#pragma endregion
+
+void Animation()
+{
+	std::string speakingFace = R"end(
+      .......
+  .:::::::::::::.
+ .::'  '''''  '::.
+ :::           :::		Welcome Agent!
+ :::           :::					Hopefully your day is going well.
+ ::'           '::					Unfortunately we can't say the same...
+: : /~~~' '~~~\ : :
+:(    *  |  *    ):
+'.:     / \     :.'
+ ':    (. .)    :'
+  '.  .:::::.  .'
+   :  <[   ]>  :
+   '.  ~:::~  .'
+     '.  '  .'
+       '''''
+)end";
+	std::string blinkingFace = R"end(
+      .......
+  .:::::::::::::.
+ .::'  '''''  '::.
+ :::           :::		Welcome Agent!
+ :::           :::					Hopefully your day is going well.
+ ::'           '::					Unfortunately we can't say the same...
+: : /~~~' '~~~\ : :
+:(    -  |  -    ):
+'.:     / \     :.'
+ ':    (. .)    :'
+  '.  .:::::.  .'
+   :  <----->  :
+   '.  ~:::~  .'
+     '.  '  .'
+       '''''
+)end";
+
+	std::string face = R"end(
+      .......          
+  .:::::::::::::.      
+ .::'  '''''  '::.     
+ :::           :::     
+ :::           :::     
+ ::'           '::     
+: : /~~~' '~~~\ : :    
+:(    *  |  *    ):    
+'.:     / \     :.'    
+ ':    (. .)    :'     
+  '.  .:::::.  .'      
+   :  <----->  :       
+   '.  ~:::~  .'       
+     '.  '  .'         
+       '''''            
+)end";
+	const char* newLine = "\n"; // used to keep the placing of string
+
+	// Resets the screen and cursor. while pasting a new substring
+	for (int i = 1; i <= 35;i++)
+	{
+		if (i % 3 == 0) std::cout << blinkingFace;
+		else std::cout << speakingFace;
+		ClearScreen(250);
+	}
+
+	std::cout << face;
+	for (int i = 1; i <= 15; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			std::cout << newLine;
+		}
+		std::cout << face.substr(24 * i, 363);
+		ClearScreen(10);
+	}
+}
+
 void PrintIntro()
 {
-	
+	Animation();
 	SetConsoleTextAttribute(coutHandle, 15);
-	std::cout << R"(
-      .......
-  .:::::::::::::.  
- .::'  '''''  '::.				
- :::           :::		Agent Welcome!
- :::           ::: 
- ::'           ':: 
-: : /~~~' '~~~\ : :
-:(    *  |  *    ):			
-'.:     / \     :.'
- ':    (. .)    :' 
-  '.  .:::::.  .'
-   :  <----->  :   
-   '.  ~:::~  .'
-     '.  '  .'     
-       '''''
-)" << "\n";
+
 	// Print welcome statement	
-	std::cout << "\nWe've heard great things about you. Let me introduce myself.\nI'm General Friedchicken.\n"
+	SlowDownText("\nWe've heard great things about you. Let me introduce myself.\nI'm General Friedchicken.\n"
 		"We called you here because we need your help to unlock our server rooms.\n"
 		"To make a long story short, an ex-employee changed all 5 server room codes.\n"
 		"He also left a set of hints for each server room to give us a chance to unlock these doors.\nThis is where you come in.\n"
 		"We heard you were really good with numbers and I hope you really are for all our sakes.\n"
-		"You will only have 3 chances per lock or else the data will delete from all the servers.\n";
+		"You will only have 5 chances per lock or else the data will delete from all the servers.\n", 2);
 	SetConsoleTextAttribute(coutHandle, 12);
-	std::cout << "Remember only 3 chances...\n";
+	SlowDownText("Remember only 5 chances...\n", 100);
 	SetConsoleTextAttribute(coutHandle, 15);
-	std::cout << "First things first we need to get you your badge. Lets get some information to put into our database and get you going.\n";
+	SlowDownText("First things first we need to get you your badge. Lets get some information to put into our database and get you going.\n", 2);
 	SetConsoleTextAttribute(coutHandle, 14);
-	std::cout << "Code name of choice: ";
+	SlowDownText("Code name of choice: ", 2);
 	std::cin >> codeName;
-	std::cout << "\nYour age: ";
+	SlowDownText("\nYour age: ", 2);
 	age = CheckIfNumber(age, "Age: ");
-	std::cout << codeName;
+	SlowDownText(codeName, 2);
 	SetConsoleTextAttribute(coutHandle, 15);
-	std::cout << ", lets get started with server room " << levelDiffuclty << "\n";
-	std::cout << "From the mission files we know:\n";
+	SlowDownText(", lets get started with server room ", 2);
+	SlowDownText(std::to_string(levelDiffuclty), 2);
+	SlowDownText("\nFrom the mission files we know:\n", 2);
+}
+void GameOver() 
+{
+	// ASCII Art needed and more 
+	SetConsoleTextAttribute(coutHandle, 12);
+	SlowDownText("\nYou have failed to enter the correct code in less than 5 turns. All data in the server room has been wiped. GAME OVER", 2);
 }
 
-void GameComplete()
+void GameWon()
 {
-	std::cout << "\Finally we have all these damn doors unlocked. Thanks for your service " << codeName << "!";
+	// ASCII Art needed and more 
+	SlowDownText("\nFinally we have all these damn doors unlocked. Thanks for your service ", 2); 
+	SlowDownText(codeName, 2); 
+	std::cout << "!";
 }
 
 bool IsPlayerAnswerCorrect(int playerSum, int playerProduct, int answerSum, int answerProduct, int maxLevel)
@@ -82,46 +170,53 @@ bool IsPlayerAnswerCorrect(int playerSum, int playerProduct, int answerSum, int 
 		if (levelDiffuclty <= maxLevel)
 		{
 			SetConsoleTextAttribute(coutHandle, 10);
-			std::cout << "The code you've entered was correct. The server lock door opens...\n"
-				"Lets move on to server room " << levelDiffuclty << "\n\n";
+			SlowDownText("The code you've entered was correct. The server lock door opens...\n"
+				"Lets move on to server room ", 2);
+			SlowDownText(std::to_string(levelDiffuclty), 2);
+			std::cout << "\n\n";
 		}
 		return true;
 	}
 	SetConsoleTextAttribute(coutHandle, 12);
 	std::cout << "The code you've entered was incorrect.\n";
+	playerGuesses++;
 	return false;
 }
 
 void GameHints(int codeSum, int codeProduct)
 {
 	//Print the hints
-	std::cout << "* There are three numbers in the code.\n"
-		<< "* If you where to multiple all the number you would get a total of " << codeProduct << ".\n"
-		<< "* The sum of the numbers in the code equals " << codeSum << "\n";
-	std::cout << "Input the correct code to enter the server room and continue...\n";
+	SlowDownText("* There are three numbers in the code.\n* The sum of the numbers in the code equals ", 2);
+	SlowDownText(std::to_string(codeSum), 2);
+	SlowDownText("\n* If you where to multiple all the number you would get a total of ", 2);
+	SlowDownText(std::to_string(codeProduct), 2);
+	SlowDownText("\nInput the correct code to enter the server room and continue...\n", 2);
 }
 
 void PlayGame()
 {
-	bool isLevelComplete;
-	int playerGuessA = 0; 
-	int playerGuessB = 0;
-	int playerGuessC = 0;
+	bool isLevelComplete = true;
+	int playerGuessA = 0, playerGuessB = 0, playerGuessC = 0;
+	int codeA = 0, codeB = 0, codeC = 0;
+	int codeSum = 0, codeProduct = 0;
 	const int maxLevel = 5;
-
-	PrintIntro();
 	
+	PrintIntro();
 
 	while (levelDiffuclty <= maxLevel)
 	{
-		//Secret code 3 number code
-		int codeA = rand() % ((4 + levelDiffuclty) * levelDiffuclty);
-		int codeB = rand() % ((4 + levelDiffuclty) * levelDiffuclty);
-		int codeC = rand() % ((4 + levelDiffuclty) * levelDiffuclty);
-		int codeSum = codeA + codeB + codeC;
-		int codeProduct = codeA * codeB * codeC;
+		if (isLevelComplete)
+		{
+			srand(int(NULL)); // Randomize rand()
+			//Secret code 3 number code
+			codeA = rand() % ((4 + levelDiffuclty) * levelDiffuclty);
+			codeB = rand() % ((4 + levelDiffuclty) * levelDiffuclty);
+			codeC = rand() % ((4 + levelDiffuclty) * levelDiffuclty);
+			codeSum = codeA + codeB + codeC;
+			codeProduct = codeA * codeB * codeC;
+			GameHints(codeSum, codeProduct);
+		}
 
-		GameHints(codeSum, codeProduct);
 		SetConsoleTextAttribute(coutHandle, 11);
 		std::cout << "First code: ";
 		playerGuessA = CheckIfNumber(playerGuessA, "First code: ");
@@ -130,29 +225,30 @@ void PlayGame()
 		std::cout << "Third code: ";
 		playerGuessC = CheckIfNumber(playerGuessC, "Third code: ");
 
-		std::cout << "\nCode entered: " << playerGuessA << playerGuessB << playerGuessC << std::endl;
+		SlowDownText("\nCode entered: ", 2);
+		std::cout << playerGuessA << playerGuessB << playerGuessC << std::endl;
 
 		int playerGuessSum = playerGuessA + playerGuessB + playerGuessC;
 		int playerGuessProduct = playerGuessA * playerGuessB * playerGuessC;
 
-		std::cout << "Player guess sum: " << playerGuessSum << std::endl;
-		std::cout << "Player guess product: " << playerGuessProduct << std::endl;
-
-		//std::cout << "Code Sum " << codeSum << std::endl;
-		//std::cout << "Code Product " << codeProduct << std::endl;
+		SlowDownText("Player guess sum: ", 2);
+		std::cout << playerGuessSum << std::endl;
+		SlowDownText("Player guess product: ", 2); 
+		std::cout << playerGuessProduct << std::endl;
 
 		isLevelComplete = IsPlayerAnswerCorrect(playerGuessSum, playerGuessProduct, codeSum, codeProduct, maxLevel);
-
+		SetConsoleTextAttribute(coutHandle, 15);
+		if (playerGuesses >= 5) break;
 		std::cin.clear();
 		std::cin.ignore();
 	}
-	GameComplete(); // Game has been beaten
+	if (playerGuesses >= 5) GameOver(); 
+	else GameWon(); 
 }
 
 
 int main()
 {
-	srand(time(NULL)); // Randomize rand()
 	PlayGame();
 
 	return 0;
